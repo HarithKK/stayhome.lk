45f86a6e09f5bd116d3432a65793db743882edc1
kolitha warnakulasooriya <kolitha.warnakulasooriya@syscolabs.com>
2020-04-06 16:31:53 +0530

    added acceptance messages

--- screens/components/Acceptance/index.js
+++ screens/components/Acceptance/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { BackHandler } from "react-native";
-import { Content, Text, View, Button } from 'native-base';
+import { BackHandler, ScrollView } from "react-native";
+import { MarkdownView } from 'react-native-markdown-view'
+import { Text, View, Button } from 'native-base';
 import Messages from '../../../utils/messages';
 
 const styles = {
-    container:{flex:1, justifyContent:'flex-start', alignItems:'center',paddingTop:40, paddingLeft:10,paddingRight:10},
+    container:{flex:1, marginTop: '5%',paddingLeft:10,paddingRight:10},
     text: {textAlign:"justify"},
     viewStyle: {marginTop: 30,flexDirection:'row', flexWrap:'wrap', marginBottom: 10},
     button: {margin: 5, width: '45%', justifyContent: 'center'}
@@ -15,10 +16,13 @@ const onHandleCancelButton = () => {
 }
 
 export default ({language, onAccepted}) => 
-    <Content contentContainerStyle={styles.container}>
-        <Text style={styles.text} >{Messages('acceptance',language)}</Text>
+    <ScrollView 
+        behaviour = "height" 
+        style= {styles.container}>
+        
+        <MarkdownView style={styles.text} >{Messages('acceptance',language)}</MarkdownView>
         <View  style={styles.viewStyle}>
             <Button style={styles.button} onPress={onAccepted}><Text >{Messages('accept',language)}</Text></Button>
             <Button style={styles.button} dark onPress={onHandleCancelButton}><Text>{Messages('quit',language)}</Text></Button>
         </View>
-    </Content>
+    </ScrollView> 
--- utils/messages.js
+++ utils/messages.js
@@ -225,9 +225,121 @@ const messages = {
         ti: 'ஏற்றுக்கொள்ளப்பட்டது'
     },
     acceptance:{
-        en: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
-        si: `ලොරම් ඉප්සම් යනු මුද්‍රණ හා යතුරු ලියන කර්මාන්තයේ ව්‍යාජ පා text යකි. ලෝරම් ඉප්සම් යනු 1500 දශකයේ සිට කර්මාන්තයේ සම්මත ව්‍යාජ පා text ය වන අතර, නාඳුනන මුද්‍රණ යන්ත්‍රයක් ගැලී වර්ගයක් ගෙන එය නිදර්ශක පොතක් සෑදීම සඳහා සීරීමට ලක් කළේය. එය ශතවර්ෂ පහක් පමණක් නොව ඉලෙක්ට්‍රොනික් යතුරුලියනය සඳහා වූ පිම්ම ද නොනැසී පවතී. 1960 දශකයේ දී ලෝරම් ඉප්සම් ඡේද අඩංගු ලෙට්‍රසෙට් ෂීට් නිකුත් කිරීමත්, මෑතකදී ඇල්ඩස් පේජ්මේකර් වැනි ඩෙස්ක්ටොප් ප්‍රකාශන මෘදුකාංග සමඟ ලොරම් ඉප්සම්ගේ අනුවාදයන්ත් සමඟ එය ජනප්‍රිය විය.`,
-        ti: `லோரெம் இப்சம் என்பது அச்சிடுதல் மற்றும் தட்டச்சு செய்யும் துறையின் போலி உரை. லோரெம் இப்சம் 1500 களில் இருந்தே தொழில்துறையின் நிலையான போலி உரையாக இருந்து வருகிறது, ஒரு அறியப்படாத அச்சுப்பொறி ஒரு வகை கேலியை எடுத்து ஒரு வகை மாதிரி புத்தகத்தை உருவாக்க அதை துருவிக் கொண்டது. இது ஐந்து நூற்றாண்டுகள் மட்டுமல்லாமல், எலக்ட்ரானிக் டைப் செட்டிங்கிற்கான பாய்ச்சலும் தப்பிப்பிழைத்தது, அடிப்படையில் மாறாமல் உள்ளது. இது 1960 களில் லோரெம் இப்சம் பத்திகளைக் கொண்ட லெட்ராசெட் தாள்களை வெளியிடுவதன் மூலம் பிரபலப்படுத்தப்பட்டது, மேலும் சமீபத்தில் லோரெம் இப்சம் பதிப்புகள் உட்பட ஆல்டஸ் பேஜ்மேக்கர் போன்ற டெஸ்க்டாப் வெளியீட்டு மென்பொருளுடன் பிரபலப்படுத்தப்பட்டது.`
+        en: `#StayHomeLK uses location information#
+
+StayHomeLK uses your device's location services to make it easier for the Srilanka Police to track your location at all times during the quarantine period. Location data helps improve our services including visualizing your location to the Srilanka Police and in delivering you the required goods you request through the application.
+We may collect location information from the time you open the StayHomeLK app until your quarantine period ends or is canceled by the Srilanka Police. This includes when your app is in the background.
+
+# Contact for Help - StayHomeLK #
+
+**THIS IS ONLY FOR DIFFICULTIES FACED IN THE MOBILE APPLICATION.**
+Contact our help team at any hour. They will guide you through and resolve your problem.
+
+Sudantha - 0715 721 744, Kolitha - 0719 559 356, Pansilu - 0777 332 170
+
+#User Terms#
+**PLEASE READ THESE USER TERMS CAREFULLY BEFORE DOWNLOADING OUR APPLICATION AND/OR USING OUR SERVICE.**
+
+User terms are there on the website [c19dms](www.c19dms.lk), mobile application(StayHomeLK) and on the leaflet provided to you by the police officer.
+
+#### Your use of the Application or the Service
+You warrant that the information you provide to StayHomeLK is accurate and complete. 
+StayHomeLK is entitled at all times to verify the information that you have provided and to refuse the Service or use of the Application without providing reasons.
+
+You may only access the Service using authorized means. It is your responsibility to check to ensure you download the correct Application for your device. StayHomeLK is not liable if you do not have a compatible mobile device or if you download the wrong version of the Application for your mobile device. Srilanka Police reserves the right to terminate the StayHomeLK Service and the use of the Application to you, should you be using the Service or Application with an incompatible or unauthorized device.
+By using the Application or the Service, you further agree that:
+
+1. You will only use the Service or download the Application for your sole, personal use and will not resell it to a third party;
+2. You will not authorize others to use your account;
+3. You will not assign or otherwise transfer your account to any other person or legal entity;
+4. You will not use an account that is subject to any rights of a person other than you without appropriate authorization;
+5. You will not use the Service or Application for unlawful purposes.
+6. You will not use the Service or Application to cause nuisance, annoyance or inconvenience;
+7. You will not impair the proper operation of the network;
+8. You will not try to harm the Service or Application in any way whatsoever;
+9. You will not copy, or distribute the Application or other StayHomeLK Content without written permission from StayHomeLK and Srilanka Police;
+10. You will provide us with whatever proof of identity we may reasonably request;
+11. You will only use an access point or 3G data account (AP) which you are authorized to use;
+12. You will not use the Service or Application with an incompatible or unauthorized device;
+13. You will comply with all applicable law from your home nation, the country, state and/or city in which you are present while using the Application or Service.
+
+__User__ means a person who accesses or uses the Service or Application.
+        `,
+        si: `#StayHomeLK මගින් ඔබ සිටින ස්ථානවල තොරතුරු භාවිතා  කරයි#
+නිරෝධායන කාල සීමාව තුළ සෑම විටම ඔබ සිටින ස්ථානය නිරීක්ෂණය කිරීම  ශ්‍රී ලංකා  පොලිසියට පහසු කිරීම සඳහා StayHomeLK ජංගම යෙදුමේ ස්ථාන දැන ගැනිමේ සේවාව භාවිතා කරයි. ඔබ සිටින  ස්ථානය ශ්‍රී ලංකා පොලිසියට පෙන්නුම් කිරීම සහ යෙදුම හරහා ඔබ ඉල්ලා සිටින අවශ්‍ය භාණ්ඩ ඔබට ලබා දීම ඇතුළුව අපගේ සේවාවන් වැඩි දියුණු කිරීමට ස්ථාන  පිළිබඳ  දත්ත උපකාරී වේ.
+ඔබ StayHomeLK යෙදුම විවෘත කළ දින සිට ඔබගේ නිරෝධායන කාලය අවසන් වන තෙක් හෝ ශ්‍රී ලංකා පොලිසිය  විසින් අවලංගු කරන තෙක් අපි  ඔබ සිටින  ස්ථාන පිළිබඳ තොරතුරු රැස්කරනු ඇත.
+
+#StayHomeLK  උදව් සඳහා  සම්බන්ධ කරගන්න#
+
+භාවිතයේ දී ඔබට ඇති වන ගැටළු සඳහා ඕනෑම වේලාවක අපගේ උපකාරක කණ්ඩායම අමතන්න. ඔවුන් ඔබට මග පෙන්වමින් ඔබේ ගැටළුව විසඳනු ඇත.
+
+සුදන්ත - 0715 721 744,  කෝලිත - 0719 559 356,  පන්සිලු - 0777 332 170
+
+#COVID-19 - නිරෝධායන කළමනාකරණ පද්ධතිය#
+**පරිශීලක නියමයන්.**
+
+අපගේ මෘදුකාංගය  බාගත කිරීමට පෙර සහ / හෝ අපගේ සේවාව භාවිතා කිරීමට පෙර මෙම පරිශීලක නියමයන් ප්‍රවේශමෙන් කියවන්න.
+පරිශීලක නියමයන් වෙබ් අඩවියේ [www.c19dms.lk](www.c19dms.lk), ජංගම යෙදුම (StayHomeLK) සහ පොලිස් නිලධාරියා විසින් ඔබට ලබා දී ඇති පත්‍රිකාවේ ඇත.
+
+#### ඔබ යෙදුම හෝ සේවාව භාවිතා කිරීම   
+ඔබ StayHomeLK වෙත සපයන තොරතුරු නිවැරදි හා සම්පූර්ණ බව ඔබ සහතික කළ යුතු ය. ඔබ විසින් සපයන ලද තොරතුරු සත්‍යාපනය කිරීමට සහ හේතු දැක්වීමකින් තොරව යෙදුමේ සේවාව හෝ භාවිතය ප්‍රතික්ෂේප කිරීමට StayHomeLK හට සෑම විටම අයිතියක් ඇත.
+    
+ඔබට මෙම සේවාව  සඳහා  පිවිසිය හැක්කේ බලයලත් ක්‍රම භාවිතා කර පමණි. ඔබගේ ස්මාර්ට් ජංගම දුරකථනය සඳහා නිවැරදි යෙදුම බාගත කර ඇත්දැයි පරීක්ෂා කිරීම ඔබේ වගකීමකි. ඔබට ස්මාර්ට් ජංගම දුරකථනයක් නොමැති නම් හෝ ඔබේ ස්මාර්ට් ජංගම දුරකථනය සඳහා යෙදුමේ වැරදි අනුවාදය බාගත කළහොත් StayHomeLK ඒ සඳහා වගකිව යුතු නොවේ. නොගැලපෙන හෝ අනවසර උපාංගයක් සමඟ ඔබ සේවාව හෝ යෙදුම භාවිතා කරන්නේ නම්, StayHomeLK සේවාව අවසන් කිරීමේ අයිතිය ශ්‍රී ලංකා පොලිසිය සතුය.
+
+යෙදුම හෝ සේවාව භාවිතා කිරිම සඳහා, ඔබ තවදුරටත් එකඟ වන්නේනම්:
+
+1. ඔබගේ තනි, පුද්ගලික භාවිතය සඳහා යෙදුම බාගත කර සේවාව පමණක් භාවිතා කරනු ඇත එය තෙවන පාර්ශවයකට නැවත විකුණන්නේ නැත;
+2. ඔබගේ ගිණුම භාවිතා කිරීමට ඔබ අන් අයට අවසර නොදිය යුතු ය;
+3. ඔබ ඔබේ ගිණුම වෙනත් පුද්ගලයෙකුට හෝ නීතිමය ආයතනයකට පැවරීම හෝ වෙනත් ආකාරයකින් පැවරීම නොකරනු ඇත;
+4. සුදුසු අවසරයකින් තොරව ඔබ හැර වෙනත් පුද්ගලයෙකුගේ අයිතිවාසිකම්වලට යටත් වන ගිණුමක් ඔබ භාවිතා නොකරනු ඇත;
+5. නීති විරෝධී අරමුණු සඳහා ඔබ සේවාව හෝ යෙදුම භාවිතා නොකරනු ඇත;
+6. ගැටළු හෝ අපහසුතාවයන් ඇති කිරීමට ඔබ සේවාව හෝ යෙදුම භාවිතා නොකරනු ඇත;
+7. ජාලයේ නිසි ක්‍රියාකාරිත්වය ඔබ  බාධාවක් නොවනු ඇත;
+8. සේවයට හෝ යෙදුමට කිසිදු ආකාරයකින් හානි කිරීමට ඔබ උත්සාහ නොකරනු ඇත;
+9. ශ්‍රී ලංකා  පොලිසියේ ලිඛිත අවසරයකින් තොරව ඔබ යෙදුම හෝ වෙනත් StayHomeLK අන්තර්ගතයන් පිටපත් කිරීම හෝ බෙදා හැරීම නොකරනු ඇත;
+10. අප සාධාරණ ලෙස ඉල්ලා සිටින අනන්‍යතාවය පිළිබඳ ඕනෑම සාක්ෂියක් ඔබ අපට ලබා දෙනු ඇත;
+11. අප සාධාරණ ලෙස ඉල්ලා සිටින අනන්‍යතාවය පිළිබඳ ඕනෑම සාක්ෂියක් ඔබ අපට ලබා දෙනු ඇත;  
+12. නොගැලපෙන හෝ අනවසර උපාංගයක් සමඟ ඔබ සේවාව හෝ යෙදුම භාවිතා නොකරනු ඇත; 
+13. යෙදුම හෝ සේවාව භාවිතා කරන අතරතුර ඔබ සිටින ඔබගේ මව් රට, ප්‍රාන්තය සහ / හෝ නගරයට අදාළ වන සියලුම නීතිය ඔබ අනුකූල වනු ඇත.
+
+__පරිශීලකයා__ යනු සේවාවට හෝ යෙදුමට ප්‍රවේශ වන හෝ භාවිතා කරන පුද්ගලයෙකි.`,
+        ti: `#StayHomeLK இருப்பிட தகவலைப் பயன்படுத்துகிறது.#
+தனிமைப்படுத்தப்பட்ட காலத்தில் எல்லா நேரங்களிலும் உங்கள் இருப்பிடத்தைக் கண்காணிக்கும் பணியை இலங்கை காவல்துறையினருக்கு எளிதாக்குவதற்கு StayHomeLK உங்கள் சாதனத்தின் இருப்பிட சேவைகளைப் பயன்படுத்துகிறது. உங்கள் இருப்பிடத்தை இலங்கை காவல்துறைக்கு காட்சிப்படுத்துவது மற்றும் விண்ணப்பத்தின் மூலம் நீங்கள் கோரிய தேவையான பொருட்களை உங்களுக்கு வழங்குவது உள்ளிட்ட எங்கள் சேவைகளை மேம்படுத்த இருப்பிட தரவு உதவுகிறது.
+நீங்கள் StayHomeLK பயன்பாட்டைத் திறந்த நேரத்திலிருந்து உங்கள் தனிமைப்படுத்தப்பட்ட காலம் முடிவடையும் வரை அல்லது இலங்கை காவல்துறையால் ரத்து செய்யப்படும் வரை இருப்பிடத் தகவல்களை நாங்கள் சேகரிக்கலாம். உங்கள் பயன்பாடு பின்னணியில் வேலை செய்யும் போதும் இது சேகரிக்கப்படும்.
+        
+#உதவிக்கு தொடர்பு கொள்ளுங்கள் - StayHomeLK#
+
+இது மொபைல் பயன்பாட்டினை பயன்படுத்தும் போது உள்ள குறைபாடுகளுக்கு மட்டுமே.
+எந்த நேரத்திலும் எங்கள் உதவி குழுவைத் தொடர்பு கொள்ளுங்கள். அவர்கள் உங்களுக்கு வழிகாட்டுவர் மற்றும் உங்கள் பிரச்சினையை தீர்ப்பார்கள்.
+
+சுதந்த - 0715721744, கொலித்த - 0719559356, பன்சிலு - 0777332170
+
+#பயனர் விதிமுறைகள்.#
+**எங்கள் பயன்பாடைப் பதிவிறக்குவதற்கு முன் அல்லது எங்கள் சேவையைப் பயன்படுத்துவதற்கு முன்பு இந்த பயனர் விதிமுறைகளை கவனமாகப் படிக்கவும்.**
+
+பயனர் விதிமுறைகள் இணையதளத்தில் [c19dms](www.c19dms.lk), மொபைல் பயன்பாடில் (StayHomeLK) மற்றும் போலீஸ் அதிகாரி உங்களுக்கு வழங்கிய துண்டுப்பிரசுரத்தில் உள்ளன நீங்கள் இந்த சேவையினை அல்லது பயன்பாட்டைப் பயன்படுத்துவதன் மூலம்.
+நீங்கள் StayHomeLK க்கு வழங்கும் தகவல்கள் துல்லியமானவை மற்றும் முழுமையானவை என்று நீங்கள் உத்தரவாதம் அளிக்கிறீர்கள்.
+நீங்கள் வழங்கிய தகவல்களைச் சரிபார்க்கவும், காரணங்களை வழங்காமல் பயன்பாட்டின் சேவையை அல்லது பயன்பாட்டை மறுக்கவும் StayHomeLK க்கு எல்லா நேரங்களிலும் உரிமை உண்டு.
+அங்கீகரிக்கப்பட்ட வழிகளைப் பயன்படுத்தி மட்டுமே நீங்கள் சேவையை அணுக முடியும். உங்கள் சாதனத்திற்கான சரியான பயன்பாட்டைப் பதிவிறக்குவதை உறுதிசெய்வது உங்கள் பொறுப்பாகும். உங்களிடம் இணக்கமான மொபைல் சாதனம் இல்லையென்றால் அல்லது உங்கள் மொபைல் சாதனத்திற்கான பயன்பாட்டின் தவறான பதிப்பை பதிவிறக்கம் செய்தால் StayHomeLK பொறுப்பல்ல. நீங்கள் பொருந்தாத அல்லது அங்கீகரிக்கப்படாத சாதனத்துடன் சேவை அல்லது விண்ணப்பத்தைப் பயன்படுத்துகிறீர்களானால், StayHomeLK சேவையை நிறுத்துவதற்கான உரிமை இலங்கை காவல்துறை கொண்டுள்ளது.
+பயன்பாடு அல்லது அதன் சேவையைப் பயன்படுத்துவதன் மூலம், நீங்கள் இதை மேலும் ஒப்புக்கொள்கிறீர்கள்:
+
+1. நீங்கள் தனிப்பட்ட பயன்பாட்டிற்காக மட்டும் பயன்பாட்டைப்    பதிவிறக்குவீர்கள், அதை மூன்றாம் தரப்பினருக்கு மறுவிற்பனை செய்ய மாட்டீர்கள்;
+2. உங்கள் கணக்கைப் பயன்படுத்த மற்றவர்களுக்கு நீங்கள்  அங்கீகாரம் வழங்க மாட்டீர்கள்;
+3. உங்கள் கணக்கை வேறு எந்த நபருக்கும் அல்லது சட்டப்பூர்வ நிறுவனத்திற்கும் நீங்கள் உபயோகிக்க அனுமதி கொடுக்கவோ அல்லது மாற்றவோ மாட்டீர்கள்;
+4. உங்களைத் தவிர வேறு நபரின் எந்தவொரு உரிமைகளுக்கும் உட்பட்ட கணக்கை உரிய அங்கீகாரமின்றி நீங்கள் பயன்படுத்த மாட்டீர்கள்;
+5. நீங்கள் சட்டவிரோத நோக்கங்களுக்காக சேவை அல்லது பயன்பாட்டை பயன்படுத்த மாட்டீர்கள்.
+6. தொல்லை, எரிச்சல் அல்லது சிரமத்தை ஏற்படுத்த நீங்கள் சேவையையோ பயன்பாட்டையோ பயன்படுத்த மாட்டீர்கள்;
+7. வலைப்பின்னலின் சரியான செயல்பாட்டை நீங்கள் பாதிக்க முயற்சிக்க மாட்டீர்கள்;
+8. சேவை அல்லது பயன்பாட்டிற்கு எந்த வகையிலும் தீங்கு விளைவிக்க நீங்கள் முயற்சிக்க மாட்டீர்கள்;
+9. StayHomeLK மற்றும் இலங்கை காவல்துறையினரின் எழுத்துப்பூர்வ அனுமதியின்றி நீங்கள் பயன்பாடு அல்லது பிற StayHomeLK உள்ளடக்கத்தை நகலெடுக்கவோ விநியோகிக்கவோ மாட்டீர்கள்;
+10.நாங்கள் நியாயமான முறையில் கோரக்கூடிய அடையாளத்திற்கான எந்த ஆதாரத்தையும் நீங்கள் எங்களுக்கு வழங்குவீர்கள்;
+11. நீங்கள் பயன்படுத்த அங்கீகாரம் பெற்ற அணுகல் புள்ளி அல்லது 3G தரவுக் கணக்கை (AP) மட்டுமே பயன்படுத்துவீர்கள்;
+12. பொருந்தாத அல்லது அங்கீகரிக்கப்படாத சாதனத்துடன் நீங்கள் சேவையையோ பயன்பாட்டையோ பயன்படுத்த மாட்டீர்கள்;
+13. பயன்பாட்டை அல்லது சேவையைப் பயன்படுத்தும் போது நீங்கள் இருக்கும் உங்கள் சொந்த நாடு, மாநிலம் மற்றும் நகரத்திற்கு பொருந்தக்கூடிய அனைத்து சட்டங்களுக்கும் நீங்கள் இணங்குவீர்கள்.
+
+__பயனர்__ என்பது சேவை அல்லது பயன்பாட்டை அணுகும் அல்லது பயன்படுத்தும் ஒரு நபர்
+        `
+    
     }
 }
 
